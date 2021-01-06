4f225864262fdcc7b3387fd5a2de43c7bc30b2cd
AsdreDmitry <michalych2014@yandex.ru>
2021-01-06 12:49:08 +0300

    insertion_sort

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,14 +2,17 @@ package com.company;
 import java.util.Random;
 import java.util.Arrays;
 public class Main {
-
+    public static void tests_1(){
+        //
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    }
     public static void main(String[] args) {
 	// write your code here
-        int size = 10000;
+        int size = 2000;
         int[] array = new int[size];
         int[] check = new int[size];
         final Random random = new Random();
-        for(int j = 0; j < 10000; j++) {
+        for(int j = 0; j < 1000; j++) {
             for (int i = 0; i < size; i++) {
                 array[i] = random.nextInt(100);
                 check[i] = array[i];
--- src/com/company/QuickSort.java
+++ src/com/company/QuickSort.java
@@ -2,14 +2,24 @@ package com.company;
 public class QuickSort {
     public static void quick_sort(int[] arr, int from_Index, int to_Index){
         if(from_Index < to_Index){
-            int[] p = partition(arr, from_Index, to_Index);
-            //System.out.printf("_ %d %d %d %d \n",from_Index, p, p + 1, to_Index);
-            //System.out.println(arr[0]);
-            int lt = p[0];
-            int gt = p[1];
-            //System.out.printf(" %d %d \n", lt, gt);
-            quick_sort(arr, from_Index, lt);
-            quick_sort(arr, gt + 1, to_Index);
+            if (from_Index - to_Index  < 4){
+                instertion_sort(arr, from_Index, to_Index);
+            }else {
+                int[] p = partition(arr, from_Index, to_Index);
+                int lt = p[0];
+                int gt = p[1];
+                quick_sort(arr, from_Index, lt);
+                quick_sort(arr, gt + 1, to_Index);
+            }
+        }
+    }
+    private static void instertion_sort(int[] arr, int from_Index, int to_Index){
+        for(int i = from_Index + 1; i < to_Index; i++){
+            int j = i - 1;
+            while(j >= from_Index && arr[j] > arr[j + 1]){
+                int tmp = arr[j]; arr[j] = arr[j + 1]; arr[j + 1] = tmp;
+                j--;
+            }
         }
     }
     private static void median_of_three(int[] arr, int from_Index, int to_Index){
@@ -32,8 +42,6 @@ public class QuickSort {
         int tmp = arr[to_Index - 1]; arr[to_Index - 1] = arr[from_Index]; arr[from_Index] = tmp;
     }
     private static int[] partition(int[] arr, int from_Index, int to_Index){
-       // System.out.println("this is from index");
-        //System.out.println(from_Index);
         median_of_three(arr, from_Index, to_Index);
         int pivot = arr[from_Index];
         int i = from_Index + 1, lt = from_Index + 1, gt = to_Index - 1;
@@ -43,7 +51,6 @@ public class QuickSort {
                 lt++; i++;
             }
             else if(arr[i] > pivot){
-                //System.out.println(gt);
                 int tmp = arr[i]; arr[i] = arr[gt]; arr[gt] = tmp;
                 gt--;
             }
@@ -51,8 +58,6 @@ public class QuickSort {
                 i++;
         }
         lt--;
-       // System.out.println("This is lt");
-        //System.out.println(lt);
         int tmp = arr[from_Index]; arr[from_Index] = arr[lt]; arr[lt] = tmp;
         int[] output = {lt, gt};
 
