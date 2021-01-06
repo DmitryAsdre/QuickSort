dbd1fdac488f343d1300e47cf9d22ee569b8f95e
AsdreDmitry <michalych2014@yandex.ru>
2021-01-06 11:09:08 +0300

    median_of_three

--- src/com/company/QuickSort.java
+++ src/com/company/QuickSort.java
@@ -3,14 +3,33 @@ public class QuickSort {
     public static void quick_sort(int[] arr, int from_Index, int to_Index){
         if(from_Index < to_Index){
             int p = partition(arr, from_Index, to_Index);
-            //System.out.printf("_ %d %d %d %d _",from_Index, p, p + 1, to_Index);
+            //System.out.printf("_ %d %d %d %d \n",from_Index, p, p + 1, to_Index);
             //System.out.println(arr[0]);
             quick_sort(arr, from_Index, p);
             quick_sort(arr, p + 1, to_Index);
         }
     }
+    private static void median_of_three(int[] arr, int from_Index, int to_Index){
+        int mid = from_Index + (to_Index - from_Index)/2;
+        if(arr[mid] < arr[from_Index]) {
+            int tmp = arr[from_Index];
+            arr[from_Index] = arr[mid];
+            arr[mid] = tmp;
+        }
+        if (arr[from_Index] < arr[to_Index - 1]){
+            int tmp = arr[from_Index];
+            arr[from_Index] = arr[to_Index - 1];
+            arr[to_Index - 1] = tmp;
+        }
+        if(arr[mid] < arr[to_Index - 1]){
+            int tmp = arr[mid];
+            arr[mid] = arr[to_Index - 1];
+            arr[to_Index -1] = tmp;
+        }
+    }
     private static int partition(int[] arr, int from_Index, int to_Index){
-        int pivot = arr[to_Index - 1];
+        median_of_three(arr, from_Index, to_Index);
+        double pivot = arr[to_Index - 1];
         //System.out.println(pivot);
         int i = from_Index;
         for (int j = from_Index; j < to_Index; j++){
